c2319f4a0df808c63adac6dc54ad1bd04024a651
Javier Mino <javier.svg@gmail.com>
2014-01-05 21:20:04 -0300

    JM - Added Saucelabs video to failure report

--- src/main/java/org/caponford/etsy/jbehave/ScreenshootingHtmlOutput.java
+++ src/main/java/org/caponford/etsy/jbehave/ScreenshootingHtmlOutput.java
@@ -1,27 +1,70 @@
 package org.caponford.etsy.jbehave;
 
+import org.apache.commons.codec.binary.Hex;
 import org.jbehave.core.failures.UUIDExceptionWrapper;
+import org.jbehave.core.reporters.HtmlOutput;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.web.selenium.WebDriverHtmlOutput;
+import org.jbehave.web.selenium.SauceWebDriverProvider;
 import org.jbehave.web.selenium.WebDriverProvider;
 import org.jbehave.web.selenium.WebDriverScreenshotOnFailure;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.PrintStream;
+import java.security.GeneralSecurityException;
+import java.security.Key;
 
-public class ScreenshootingHtmlOutput extends WebDriverHtmlOutput {
+public class ScreenshootingHtmlOutput extends HtmlOutput {
 
+    private final WebDriverProvider webDriverProvider;
     private WebDriverScreenshotOnFailure screenshotMaker;
 
     public ScreenshootingHtmlOutput(PrintStream output,
                                     StoryReporterBuilder reporterBuilder,
                                     WebDriverProvider webDriverProvider) {
         super(output, reporterBuilder.keywords());
+        this.webDriverProvider = webDriverProvider;
         screenshotMaker = new WebDriverScreenshotOnFailure(
-                webDriverProvider);
+                this.webDriverProvider);
+    }
+
+
+
+    private String noLoginLinkFor(SessionId jobId) {
+
+        String authCode = generateHMACFor(SauceWebDriverProvider.getSauceUser() + ":" + SauceWebDriverProvider.getSauceUser(), jobId);
+        return "https://saucelabs.com/video-embed/" + jobId + ".js?auth=" + authCode;
+    }
+
+    private String generateHMACFor(String secretKey, SessionId sessionId) {
+        try {
+            byte[] keyBytes = secretKey.getBytes();
+            Key key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "HmacMD5");
+            Mac mac = Mac.getInstance("HmacMD5");
+            mac.init(key);
+            return new String(Hex.encodeHex(mac.doFinal(sessionId.toString().getBytes())));
+        } catch (GeneralSecurityException e) {
+            throw new IllegalStateException("Could not generate HMAC for some reason", e);
+        }
+    }
+
+    private void overrideFailedLine() {
+        if (webDriverProvider.get() instanceof RemoteWebDriver) {
+            RemoteWebDriver remoteWebDriver = (RemoteWebDriver) webDriverProvider.get();
+            overwritePattern("failed",
+                    "<div class=\"step failed\">{0} <span class=\"keyword failed\">({1})</span><br/><span class=\"message failed\">{2}</span>"
+                            + "<br/><a color=\"black\" target=\"jb_scn_shot\" href=\"../screenshots/failed-scenario-{3}.png\"><img src=\"images/failing_screenshot.png\" alt=\"failing screenshot\"/></a></div>\n"
+                            + "<script src=\""
+                            + noLoginLinkFor(remoteWebDriver.getSessionId())
+                            + "\"></script>\n");
+        }
     }
 
     @Override
     public void failed(String step, Throwable storyFailure) {
+        overrideFailedLine();
         super.failed(step, storyFailure);
         try {
             UUIDExceptionWrapper uuidWrappedFailure =
